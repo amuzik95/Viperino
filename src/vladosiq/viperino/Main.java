d9cd1a2241adb0fe7bacd83536e9e573ea5ab867
amuzik95 <amuzik95@gmail.com>
2015-04-09 05:13:35 +0300

    another commit

---
+++ src/vladosiq/viperino/Main.java
@@ -0,0 +1,10 @@
+package vladosiq.viperino;
+
+/**
+ * Created by Amuzing on 09.04.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!\n");
+    }
+}
