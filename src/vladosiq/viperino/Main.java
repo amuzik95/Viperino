424948cf47b6d215871f4b60b20b7f90600f38f7
amuzik95 <amuzik95@gmail.com>
2015-04-09 05:18:26 +0300

    3rd commit testerino

--- src/vladosiq/viperino/Main.java
+++ src/vladosiq/viperino/Main.java
@@ -1,8 +1,5 @@
 package vladosiq.viperino;
 
-/**
- * Created by Amuzing on 09.04.2015.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World!\n");
