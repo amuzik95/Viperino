b9b2d8c5f9c33fa10e551bd61d414a678d26f59c
amuzik95 <amuzik95@gmail.com>
2015-04-14 06:24:40 +0300

    HERE COMES THE GRAPHICS!

--- src/vladosiq/viperino/Main.java
+++ src/vladosiq/viperino/Main.java
@@ -1,8 +1,15 @@
 package vladosiq.viperino;
 
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!\n");
+        //System.out.printf("Hello World!\n");
+        JFrame jframe = new JFrame();
+        jframe.setSize(1280,720);
+        jframe.setVisible(true);
+        jframe.setTitle("THE FATHER OF SNAKES!");
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
