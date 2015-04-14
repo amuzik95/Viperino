3e01531e22e3bbbc837baeb8483590570e8ab70b
amuzik95 <amuzik95@gmail.com>
2015-04-14 17:14:42 +0300

    HERE COMES THE GRAPHICS and rectangles (and colors)!

--- src/vladosiq/viperino/JavaRectangleDrawingComponent.java
+++ src/vladosiq/viperino/JavaRectangleDrawingComponent.java
@@ -9,7 +9,19 @@ public class JavaRectangleDrawingComponent extends JComponent {
 
         for(int i=0; i<10; ++i) {
             for(int j=0; j<20; ++j) {
-                g2D.drawRect(220+40*(j),140+40*(i),40,40);
+                if(i==5 && (j>5 && j<10)) {
+                    g2D.setColor(new Color(0,255,0));
+                    g2D.fillRect(220 + 40 * (j), 140 + 40 * (i), 40, 40);
+                }
+                else if(i==8 && j==8) {
+                    g2D.setColor(new Color(255, 120, 120));
+                    g2D.fillRect(220 + 40 * (j), 140 + 40 * (i), 40, 40);
+                }
+                else {
+                    g2D.setColor(new Color(40, 36, 33));
+                    g2D.drawRect(220 + 40 * (j), 140 + 40 * (i), 40, 40);
+                }
+
             }
         }
     }
